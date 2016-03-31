b0715d2e88d8cd92316ba00ce5556e6c5bfeadc2
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-03-31 20:47:58 +0200

    Usando la clase JsonResponse para acortar

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LuckyController
 {
@@ -28,11 +29,8 @@ class LuckyController
             'lucky_number' => rand(0, 100),
         );
 
-        return new Response(
-            json_encode($data),
-            200,
-            array('Content-Type' => 'application/json')
-        );
+        // calls json_encode and sets the Content-Type header
+        return new JsonResponse($data);
     }
 }
 ?>
